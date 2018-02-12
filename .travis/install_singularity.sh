b185275ffd68922bcaa97dbde9b6d2e6fffa0871
Juan Esteban Arango <juanes.ao@gmail.com>
2018-02-12 17:02:48 -0500

    :wrench: try singularity setup in travis without sudo

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
-sudo apt-get update && sudo apt-get install squashfs-tools dh-autoreconf build-essential
+apt-get update && apt-get install squashfs-tools dh-autoreconf build-essential
 git clone https://github.com/singularityware/singularity.git
 cd singularity
 ./autogen.sh
 ./configure --prefix=/usr/local
-make
-sudo make install
+make install
+make test
