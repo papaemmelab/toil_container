4df95340dc6a4750a18279a949b216eb96eddfc8
Juan Esteban Arango <juanes.ao@gmail.com>
2018-02-12 17:09:10 -0500

    :wrench: try singularity with chown

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -ex
 
-apt-get update && apt-get install squashfs-tools dh-autoreconf build-essential
+sudo apt-get update && sudo apt-get install squashfs-tools dh-autoreconf build-essential
 git clone https://github.com/singularityware/singularity.git
 cd singularity
 ./autogen.sh
 ./configure --prefix=/usr/local
-make install
+sudo make install
 make test
+sudo chmod g+x /usr/local/bin/singularity
