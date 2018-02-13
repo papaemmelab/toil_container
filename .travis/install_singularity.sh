db9225665edecbb71a146375613b0215a5a24cae
Juan Medina <jsmedmar@gmail.com>
2018-02-12 19:50:12 -0500

    :bug: force install singularity

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -ex
 
-if [ -f "/home/travis/build/leukgen/toil_container/singularity/bin/singularity" ]; then
-    echo exists
-else
-    apt-get update && apt-get install squashfs-tools dh-autoreconf build-essential
-    git clone https://github.com/singularityware/singularity.git
-    cd singularity
-    ./autogen.sh
-    ./configure --prefix=/home/travis/build/leukgen/toil_container/singularity
-    make install
-fi
+# if [ -f "/home/travis/build/leukgen/toil_container/singularity/bin/singularity" ]; then
+#     echo exists
+# else
+rm -rf /home/travis/build/leukgen/toil_container/singularity
+apt-get update && apt-get install squashfs-tools dh-autoreconf build-essential
+git clone https://github.com/singularityware/singularity.git
+cd singularity
+./autogen.sh
+./configure --prefix=/home/travis/build/leukgen/toil_container/singularity
+make install
+# fi
