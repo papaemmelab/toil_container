9e8777b773c7108b72932f5ed88e9aa1994e5520
Juan Esteban Arango <juanes.ao@gmail.com>
2018-02-13 13:37:56 -0500

    :wrench: fix condition

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -ex
 
+<<<<<<< HEAD
 if [ ! -x $TRAVIS_SINGULARITY_PATH/bin/singularity ]; then
     echo "Installing singularity..."
     SOURCE=/tmp/singularity_source
+=======
+if [ ! -f $TRAVIS_SINGULARITY_PATH/bin/singularity ]; then
+>>>>>>> :wrench: fix condition
     apt-get update && apt-get install squashfs-tools dh-autoreconf build-essential
     git clone https://github.com/singularityware/singularity.git $SOURCE
     cd $SOURCE
