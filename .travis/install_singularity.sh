c45101e4ab3a6d1861861ed86327d14754c9d61c
Juan Esteban Arango <juanes.ao@gmail.com>
2018-02-13 14:15:23 -0500

    :wrench: add singularity cache again

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -ex
 
-apt-get update && apt-get install squashfs-tools dh-autoreconf build-essential
-git clone https://github.com/singularityware/singularity.git
-cd singularity
-git checkout tags/$SINGULARITY_VERSION
-./autogen.sh
-./configure --prefix=$TRAVIS_SINGULARITY_PATH
-make install
+if [ ! -x $TRAVIS_SINGULARITY_PATH/singularity/bin/singularity ]; then
+    echo "Installing singularity..."
+    apt-get update && apt-get install squashfs-tools dh-autoreconf build-essential
+    git clone https://github.com/singularityware/singularity.git
+    cd singularity
+    git checkout tags/$SINGULARITY_VERSION
+    ./autogen.sh
+    ./configure --prefix=$TRAVIS_SINGULARITY_PATH
+    make install
+else
+    echo "Singularity is already installed."
+fi
