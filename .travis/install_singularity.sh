636ab93f9af7e3b2299594ae02eb05cb0eac6c1b
Juan Medina <jsmedmar@gmail.com>
2018-02-12 19:40:18 -0500

    :bug: install singularity without sudo

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -1,14 +1,12 @@
 #!/bin/bash -ex
 
-export PATH=$HOME/singularity/bin:$PATH
-
-if which singularity > /dev/null; then
-    echo exists
-else
-    sudo apt-get update && sudo apt-get install squashfs-tools dh-autoreconf build-essential
-    git clone https://github.com/singularityware/singularity.git
-    cd singularity
-    ./autogen.sh
-    ./configure --prefix=$HOME/singularity
-    sudo make install
-fi
+# if [ -f "$HOME/singularity/bin/singularity" ]; then
+#     echo exists
+# else
+apt-get update && apt-get install squashfs-tools dh-autoreconf build-essential
+git clone https://github.com/singularityware/singularity.git
+cd singularity
+./autogen.sh
+./configure --prefix=$HOME/singularity
+make install
+# fi
