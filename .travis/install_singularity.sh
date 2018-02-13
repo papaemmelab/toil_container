869d9efb9e9dae0d5e4ee91294f05d30d8c8ad78
Juan Medina <jsmedmar@gmail.com>
2018-02-12 19:16:23 -0500

    :bug: try to cache singularity

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -ex
 
-sudo apt-get update && sudo apt-get install squashfs-tools dh-autoreconf build-essential
-git clone https://github.com/singularityware/singularity.git
-cd singularity
-./autogen.sh
-./configure --prefix=/usr/local
-sudo make install
+export PATH=$HOME/singularity/bin:$PATH
+
+if which singularity > /dev/null; then
+    echo exists
+else
+    sudo apt-get update && sudo apt-get install squashfs-tools dh-autoreconf build-essential
+    git clone https://github.com/singularityware/singularity.git
+    cd singularity
+    ./autogen.sh
+    ./configure --prefix=$HOME/singularity
+    sudo make install
+fi
