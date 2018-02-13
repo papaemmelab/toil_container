8b5c33db76348130d437524d275cb6eadd326ec4
Juan Medina <jsmedmar@gmail.com>
2018-02-13 15:05:12 -0500

    :bug: fix condition in install_singularity.sh

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -ex
 
-if [ ! -x $TRAVIS_SINGULARITY_PATH/singularity/bin/singularity ]; then
+if [ ! -x $TRAVIS_SINGULARITY_PATH/bin/singularity ]; then
     echo "Installing singularity..."
+    SOURCE=/tmp/singularity_source
     apt-get update && apt-get install squashfs-tools dh-autoreconf build-essential
-    git clone https://github.com/singularityware/singularity.git
-    cd singularity
+    git clone https://github.com/singularityware/singularity.git $SOURCE
+    cd $SOURCE
     git checkout tags/$SINGULARITY_VERSION
     ./autogen.sh
     ./configure --prefix=$TRAVIS_SINGULARITY_PATH
     make install
+    rm -rf $SOURCE
 else
     echo "Singularity is already installed."
 fi
