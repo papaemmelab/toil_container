e7501fbb5f05718c804be48db5b1348da9669ca1
Juan Medina <jsmedmar@users.noreply.github.com>
2018-02-12 20:36:12 -0500

    Update install_singularity.sh

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -ex
 
 #if [ -f "/home/travis/build/leukgen/toil_container/singularity/bin/singularity" ]; then
-    sudo rm -rf /home/travis/build/leukgen/toil_container/singularity
+    #sudo rm -rf /home/travis/build/leukgen/toil_container/singularity
     apt-get update && apt-get install squashfs-tools dh-autoreconf build-essential
     git clone https://github.com/singularityware/singularity.git
     cd singularity
     ./autogen.sh
-    ./configure --prefix=/home/travis/build/leukgen/toil_container/singularity
+    ./configure --prefix=/home/travis/build/leukgen/toil_container/singularity2
     make install
 #else
     echo exists
