0254bef4711634b6b966a66bf8f0a0e6ab057b05
Juan Medina <jsmedmar@users.noreply.github.com>
2018-02-15 16:59:02 -0500

    :wrench: remove unused imports in __init__.py

--- toil_container/__init__.py
+++ toil_container/__init__.py
@@ -1,19 +1,16 @@
 """toil_container module."""
 
-from os.path import join
-from os.path import abspath
-from os.path import dirname
+from toil_container.jobs import (
+    ContainerCallJob
+)
 
-from toil_container.jobs import ContainerCallJob
 from toil_container.parsers import (
     ContainerArgumentParser,
     ContainerShortArgumentParser,
     ToilShortArgumentParser
 )
 
-MODULE_ROOT = abspath(dirname(__file__))
-
-with open(join(MODULE_ROOT, "VERSION"), "r") as f:
+with open("VERSION", "r") as f:
     VERSION = f.read().strip()
 
 __version__ = VERSION
