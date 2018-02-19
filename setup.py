ea4823f779c16c6e8275e6f08cd4794c64c642e0
Travis CI <travis@travis-ci.org>
2018-02-19 11:04:03 -0500

    :wrench: don't use README as long_description (as for now)

--- setup.py
+++ setup.py
@@ -3,16 +3,10 @@
 from os.path import abspath
 from os.path import dirname
 from os.path import join
-import io
 import json
 
 from setuptools import find_packages
 from setuptools import setup
-
-
-def read(path, **kwargs):
-    """Return content of a file."""
-    return io.open(path, encoding=kwargs.get("encoding", "utf8")).read()
 
 # make sure we use absolute paths
 ROOT = abspath(dirname(__file__))
@@ -21,20 +15,21 @@ ROOT = abspath(dirname(__file__))
 with open(join(ROOT, "setup.json"), "r") as f:
     SETUP = json.load(f)
 
-setup(
-    # load description from README
-    long_description=read(join(ROOT, "README.md")),
+# see 4 > https://packaging.python.org/guides/single-sourcing-package-version/
+with open(join(ROOT, "{{cookiecutter.project_slug}}", "VERSION"), "r") as f:
+    VERSION = f.read().strip()
 
-    # in combination with MANIFEST.in, non-python files included
-    # inside the toil_container will be copied to the
-    # site-packages and wheels installation directory
+setup(
+    # single source package version
+    version=VERSION,
+
+    # in combination with recursive-includes in MANIFEST.in, non-python files
+    # included inside the {{cookiecutter.project_slug}} will be copied to the
+    # site-packages and wheels installation directories
     include_package_data=True,
 
     # return a list all Python packages found within the ROOT directory
     packages=find_packages(),
-
-    # The version is only defined in one place
-    version=read(join(ROOT, "toil_container", "VERSION")).strip(),
 
     # pass parameters loaded from setup.json including author and version
     **SETUP
