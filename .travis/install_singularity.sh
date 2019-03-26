64ed690d9bcc9a63b9ee0934cbbb59f919ad7b8c
Juan Medina <juansantiago.medina@gmail.com>
2019-03-26 11:12:03 -0400

    🔧 minor change

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -3,8 +3,6 @@
 if [ ! -x $TRAVIS_SINGULARITY_PATH/bin/singularity ]; then
     echo "Installing singularity..."
     SOURCE=/tmp/singularity_source
-    CMD="apt-get update && sudo apt-get install -y python libarchive-dev squashfs-tools dh-autoreconf build-essential"
-    echo $CMD
     sudo apt-get update && sudo apt-get install -y python libarchive-dev squashfs-tools dh-autoreconf build-essential
     git clone https://github.com/singularityware/singularity.git $SOURCE
     cd $SOURCE
